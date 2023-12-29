29525855d19980969bc886a6f123858c7715fd18
KauaOnorato <kauaonorato.dev@gmail.com>
2023-12-28 20:52:30 -0400

    Primeiro commit

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+//Importações de plugins
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+
+function styles() {
+    return gulp.src('./src/styles/*.scss')
+        .pipe(sass({outputStyle : 'compressed'}))
+        .pipe(gulp.dest('./dist/styles'))
+}
+
+function image() {
+    return gulp.src('./src/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'))
+}
+
+exports.default = gulp.parallel(styles, image, scripts);
+exports.watch = function() {
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
+    gulp.watch('./src/images/**/*', gulp.parallel(image));
+}
